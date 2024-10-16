291c58e3032e91e89a6e6089c09d33af72e42f8a
Алексей <@DESKTOP-SJ0VTJH>
2024-10-16 18:29:31 +0300

    Add project files.

---
+++ ASPN1/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using ASPN1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPN1.Controllers;
+
+public class StudentController : Controller
+{
+    public Student _student = new Student
+    {
+        DateOfBirthday = DateTime.Now,
+        FirstName = "Test",
+        LastName = "Test",
+        GPA = 5
+    };
+
+    public IActionResult GetStudents()
+    {
+        //ViewBag.Student = _student;
+        //ViewData["Student"] = _student;
+        return View(_student);
+    }
+
+}
---
+++ ASPN1/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace ASPN1.Models;
+
+public class Student
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirthday { get; set; }
+
+    public double GPA { get; set; }
+
+
+}
